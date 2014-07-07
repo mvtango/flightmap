9fe08d95387ca51c9604a2208abb10e426c23f68
OpenDataCloud <opendatacity@opendatacloud.de>
2014-07-07 09:41:42 +0200

    Irgendeine Verbesserung ;-)

--- flightradar24/compress.sh
+++ flightradar24/compress.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 cd `dirname $0`
-find dumps dumps-jsonp  dumps-jsonp-filtered -type f ! -name "*.bz2" ! -name "*.7z" -mtime +2 -print |  xargs -I {} 7za a {}.7z {} && rm {} >compress.log 2>&1 
+find dumps dumps-jsonp  dumps-jsonp-filtered -type f ! -name "*.bz2" ! -name "*.7z" -mtime +2 -print |  xargs -I {} sh -c "7za a {}.7z {} && rm {}"  2>&1 >compress.log
