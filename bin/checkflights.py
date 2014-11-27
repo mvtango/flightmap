58c159027b2f551235b53dcce255abcc73c1a724
OpenDataCloud <opendatacity@opendatacloud.de>
2014-11-28 00:42:27 +0100

    output datapoints per flight

---
+++ bin/checkflights.py
@@ -0,0 +1,8 @@
+import sys
+import json
+
+
+for l in sys.stdin.readlines() :
+	o=json.loads(l)
+	print "{0}-{1} {o[start][airport]} {o[hex]}".format(o["profile"][-1]["t"],o["profile"][0]["t"],**locals())
+
